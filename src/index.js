9bf29851e692ced37d0b449592f5e6e63b134a43
Valeria Torgonina <lovec.cnov@mail.ru>
2019-11-21 16:46:41 +0300

    create index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 import {fonts} from './fonts/index';
+import ShipChartScreen from './modules/main';
+import { RootReducer, initialState } from './core/RootReducer';
+
+export const store = createStore(RootReducer ,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 class App extends React.Component {
     render () {
-       return 'hello, World'
+       return <ShipChartScreen/>
     }
    }
 
-ReactDOM.render(<App style={fonts}/>, document.getElementById('root'));
+ReactDOM.render(    
+    <Provider store={store}>
+            <App style={fonts}></App>
+    </Provider>,
+    document.getElementById('root')
+);
 
